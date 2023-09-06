92701b2f1418fbcfb8292460b90128626bc38e29
viral32111 <19510403+viral32111@users.noreply.github.com>
2023-09-07 00:05:02 +0100

    Install Prettier & Setup ESLint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,17 @@ export default function Home() {
 			<div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
 				<p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
 					Get started by editing&nbsp;
-					<code className="font-mono font-bold">src/app/page.tsx</code>
+					<code className="font-mono font-bold">
+						src/app/page.tsx
+					</code>
 				</p>
 				<div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
 					<a
 						className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
 						href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
 						target="_blank"
-						rel="noopener noreferrer"
-					>
-						By{' '}
+						rel="noopener noreferrer">
+						By{" "}
 						<Image
 							src="/vercel.svg"
 							alt="Vercel Logo"
@@ -44,16 +45,16 @@ export default function Home() {
 					href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
 					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
 					target="_blank"
-					rel="noopener noreferrer"
-				>
+					rel="noopener noreferrer">
 					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Docs{' '}
+						Docs{" "}
 						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
 							-&gt;
 						</span>
 					</h2>
 					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Find in-depth information about Next.js features and API.
+						Find in-depth information about Next.js features and
+						API.
 					</p>
 				</a>
 
@@ -61,16 +62,16 @@ export default function Home() {
 					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
 					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
 					target="_blank"
-					rel="noopener noreferrer"
-				>
+					rel="noopener noreferrer">
 					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Learn{' '}
+						Learn{" "}
 						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
 							-&gt;
 						</span>
 					</h2>
 					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Learn about Next.js in an interactive course with&nbsp;quizzes!
+						Learn about Next.js in an interactive course
+						with&nbsp;quizzes!
 					</p>
 				</a>
 
@@ -78,10 +79,9 @@ export default function Home() {
 					href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
 					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
 					target="_blank"
-					rel="noopener noreferrer"
-				>
+					rel="noopener noreferrer">
 					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Templates{' '}
+						Templates{" "}
 						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
 							-&gt;
 						</span>
@@ -95,16 +95,16 @@ export default function Home() {
 					href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
 					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
 					target="_blank"
-					rel="noopener noreferrer"
-				>
+					rel="noopener noreferrer">
 					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Deploy{' '}
+						Deploy{" "}
 						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
 							-&gt;
 						</span>
 					</h2>
 					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Instantly deploy your Next.js site to a shareable URL with Vercel.
+						Instantly deploy your Next.js site to a shareable URL
+						with Vercel.
 					</p>
 				</a>
 			</div>
